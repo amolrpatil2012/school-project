ddbf0628b43635e274dcf35cf8b0b22e57cd417c
AMOL PATIL <136699553+amolrpatil2012@users.noreply.github.com>
2023-07-17 19:55:28 +0530

    Add files via upload

---
+++ 9 . Using Lombok Lib/school/school/src/main/java/com/example/school/controller/HolidayController.java
@@ -0,0 +1,37 @@
+package com.example.school.controller;
+
+import com.example.school.model.Holiday;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+
+import java.util.Arrays;
+import java.util.List;
+import java.util.stream.Collectors;
+
+@Controller
+public class HolidayController {
+
+    @GetMapping("/holidays")
+    public String displayHolidays(Model model)
+    {
+        List<Holiday> holidays = Arrays.asList(
+
+                new Holiday("Jan 1","New Year", Holiday.Type.FESTIVAL),
+                new Holiday("Oct 31","Halloween", Holiday.Type.FESTIVAL),
+                new Holiday("Nov 24","Thanks Giving Day", Holiday.Type.FESTIVAL),
+                new Holiday("Dec 25","Chrismas", Holiday.Type.FESTIVAL),
+                new Holiday("Jan 17","Martin Luthar", Holiday.Type.FEDERAL),
+                new Holiday("July 4","Independence Day", Holiday.Type.FEDERAL),
+                new Holiday("Sep 5","Labor day", Holiday.Type.FEDERAL),
+                new Holiday("Nov 11","Veterns Day", Holiday.Type.FEDERAL)
+        );
+
+        Holiday.Type[] types = Holiday.Type.values();
+        for (Holiday.Type type : types) {
+            model.addAttribute(type.toString(),
+                    (holidays.stream().filter(holiday -> holiday.getType().equals(type)).collect(Collectors.toList())));
+        }
+        return "holidays.html";
+    }
+}
---
+++ 9 . Using Lombok Lib/school/school/src/main/java/com/example/school/model/Contact.java
@@ -0,0 +1,11 @@
+package com.example.school.model;
+
+import lombok.Data;
+
+@Data
+public class Contact {
+
+    private String name,mobileNum,email,subject,message;
+
+
+}
